06df89697a7da0b97c206bd9c0572aebfc01f1f9
Pavel Vyskočil <vyskocilpavel@muni.cz>
2019-04-09 09:27:26 +0200

    Fix the indentation

--- lib/Auth/Process/statistics.php
+++ lib/Auth/Process/statistics.php
@@ -25,25 +25,25 @@ class Statistics extends \SimpleSAML\Auth\ProcessingFilter
         $this->reserved = (array)$reserved;
     }
 
-	public function process(&$request)
-	{
-		$dateTime = new \DateTime();
-		DatabaseCommand::insertLogin($request, $dateTime);
+    public function process(&$request)
+    {
+        $dateTime = new \DateTime();
+        DatabaseCommand::insertLogin($request, $dateTime);
 
-		$eduPersonUniqueId = $request['Attributes']['eduPersonUniqueId'][0];
-		$spEntityId = $request['SPMetadata']['entityid'];
-		$sourceIdPEppn = $request['Attributes']['sourceIdPEppn'][0];
-		$sourceIdPEntityId = $request['Attributes']['sourceIdPEntityID'][0];
+        $eduPersonUniqueId = $request['Attributes']['eduPersonUniqueId'][0];
+        $spEntityId = $request['SPMetadata']['entityid'];
+        $sourceIdPEppn = $request['Attributes']['sourceIdPEppn'][0];
+        $sourceIdPEntityId = $request['Attributes']['sourceIdPEntityID'][0];
 
-		if (isset($request['perun']['user'])) {
-			$user = $request['perun']['user'];
-			Logger::notice('UserId: ' . $user->getId() . ', identity: ' .  $eduPersonUniqueId . ', service: ' . $spEntityId .
-				', external identity: ' . $sourceIdPEppn . ' from ' . $sourceIdPEntityId);
-		} else {
-			Logger::notice('User identity: ' .  $eduPersonUniqueId . ', service: ' . $spEntityId .
-				', external identity: ' . $sourceIdPEppn . ' from ' . $sourceIdPEntityId);
-		}
+        if (isset($request['perun']['user'])) {
+            $user = $request['perun']['user'];
+            Logger::notice('UserId: ' . $user->getId() . ', identity: ' .  $eduPersonUniqueId . ', service: ' . $spEntityId .
+                ', external identity: ' . $sourceIdPEppn . ' from ' . $sourceIdPEntityId);
+        } else {
+            Logger::notice('User identity: ' .  $eduPersonUniqueId . ', service: ' . $spEntityId .
+                ', external identity: ' . $sourceIdPEppn . ' from ' . $sourceIdPEntityId);
+        }
 
-	}
+    }
 
 }
