20d4f4a4adb2fc4ce6117b328a9d1f853de1fb55
Pavel Břoušek <433364@mail.muni.cz>
2019-06-24 14:14:34 +0200

    JS style, selectHandler

--- www/index.js
+++ www/index.js
@@ -1,319 +1,305 @@
+'use strict';
+
 /* global google */
-/* eslint-disable */
-if(!Function.prototype.bind){(function(){var slice=Array.prototype.slice.call.bind(Array.prototype.slice);Function.prototype.bind=function(){var thatFunc=this,thatArg=arguments[0];var args=slice(arguments,1);if(typeof thatFunc!=='function'){throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable')}return function(){var funcArgs=args.concat(slice(arguments));return thatFunc.apply(thatArg,funcArgs)}}})()}
-/* eslint-enable */
 
 function getStatisticsData(name) {
-    return $.parseJSON($('#'+name).attr('content'));
+  return $.parseJSON($('#' + name).attr('content'));
 }
 
 function getStatisticsDataYMDC(name) {
-	return getStatisticsData(name).map(function(item){
-		return [new Date(item.year, item.month-1, item.day), {v:item.count}];
-	});
+  return getStatisticsData(name).map(function mapItemToDate(item) {
+    return [new Date(item.year, item.month - 1, item.day), { v: item.count }];
+  });
 }
 
 function getTranslation(str) {
-	return $.parseJSON($('#translations').attr('content'))[str];
+  return $.parseJSON($('#translations').attr('content'))[str];
+}
+
+function selectHandler(obj, data, url) {
+  var selection = obj.getSelection();
+  if (selection.length) {
+    var id = data.getValue(selection[0].row, 1);
+    window.location.href = url + id;
+  }
 }
 
 function drawLoginsChart(getEl) {
-    var el = getEl();
-    if (!el) return;
+  var el = getEl();
+  if (!el) return;
 
-    var data = google.visualization.arrayToDataTable([['Date', 'Count']].concat(getStatisticsDataYMDC('loginCountPerDay')));
+  var data = google.visualization.arrayToDataTable([['Date', 'Count']].concat(getStatisticsDataYMDC('loginCountPerDay')));
 
-    var dashboard = new google.visualization.Dashboard(el);
+  var dashboard = new google.visualization.Dashboard(el);
 
-    var chartRangeFilter = new google.visualization.ControlWrapper({
-        'controlType': 'ChartRangeFilter',
-        'containerId': 'control_div',
-        'options': {
-            'filterColumnLabel': 'Date'
-        }
-    });
-    var chart = new google.visualization.ChartWrapper({
-        'chartType': 'LineChart',
-        'containerId': 'line_div',
-        'options': {
-            'legend': 'none'
-        }
-    });
-    dashboard.bind(chartRangeFilter, chart);
-    dashboard.draw(data);
+  var chartRangeFilter = new google.visualization.ControlWrapper({
+    controlType: 'ChartRangeFilter',
+    containerId: 'control_div',
+    options: {
+      filterColumnLabel: 'Date'
+    }
+  });
+  var chart = new google.visualization.ChartWrapper({
+    chartType: 'LineChart',
+    containerId: 'line_div',
+    options: {
+      legend: 'none'
+    }
+  });
+  dashboard.bind(chartRangeFilter, chart);
+  dashboard.draw(data);
 }
 
 function drawIdpsChart(getEl) {
-    var el = getEl();
-    if (!el) return;
+  var el = getEl();
+  if (!el) return;
 
-    var data = google.visualization.arrayToDataTable([['sourceIdpName', 'sourceIdPEntityId', 'Count']].concat(getStatisticsData('loginCountPerIdp')));
+  var data = google.visualization.arrayToDataTable([['sourceIdpName', 'sourceIdPEntityId', 'Count']].concat(getStatisticsData('loginCountPerIdp')));
 
-    data.sort([{column: 2, desc: true}]);
+  data.sort([{ column: 2, desc: true }]);
 
-    var view = new google.visualization.DataView(data);
+  var view = new google.visualization.DataView(data);
 
-    view.setColumns([0, 2]);
+  view.setColumns([0, 2]);
 
-    var options = {
-        pieSliceText: 'value',
-        chartArea: {left: 20, top: 0, width: '100%', height: '100%'},
-    };
-
-    var chart = new google.visualization.PieChart(el);
-
-    chart.draw(view, options);
-
-    google.visualization.events.addListener(chart, 'select', selectHandler);
-
-    function selectHandler() {
-        var selection = chart.getSelection();
-        if (selection.length) {
-            var entityId = data.getValue(selection[0].row, 1);
-            window.location.href = 'idpDetail.php?entityId=' + entityId;
-        }
+  var options = {
+    pieSliceText: 'value',
+    chartArea: {
+      left: 20, top: 0, width: '100%', height: '100%'
     }
+  };
+
+  var chart = new google.visualization.PieChart(el);
+
+  chart.draw(view, options);
+
+  var sh = selectHandler.bind(null, chart, data, 'idpDetail.php?entityId=');
+  google.visualization.events.addListener(chart, 'select', sh);
 }
 
 function drawSpsChart(getEl) {
-    var el = getEl();
-    if (!el) return;
-    
-    var data = google.visualization.arrayToDataTable([['service', 'serviceIdentifier', 'Count']].concat(getStatisticsData('accessCountPerService')));
+  var el = getEl();
+  if (!el) return;
 
-    data.sort([{column: 2, desc: true}]);
+  var data = google.visualization.arrayToDataTable([['service', 'serviceIdentifier', 'Count']].concat(getStatisticsData('accessCountPerService')));
 
-    var view = new google.visualization.DataView(data);
+  data.sort([{ column: 2, desc: true }]);
 
-    view.setColumns([0, 2]);
+  var view = new google.visualization.DataView(data);
 
-    var options = {
-        pieSliceText: 'value',
-        chartArea: {left: 20, top: 0, width: '100%', height: '100%'},
-    };
+  view.setColumns([0, 2]);
 
-    var chart = new google.visualization.PieChart(el);
-
-    chart.draw(view, options);
-
-    google.visualization.events.addListener(chart, 'select', selectHandler);
-
-    function selectHandler() {
-        var selection = chart.getSelection();
-        if (selection.length) {
-            var identifier = data.getValue(selection[0].row, 1);
-            window.location.href = 'spDetail.php?identifier=' + identifier;
-        }
+  var options = {
+    pieSliceText: 'value',
+    chartArea: {
+      left: 20, top: 0, width: '100%', height: '100%'
     }
+  };
+
+  var chart = new google.visualization.PieChart(el);
+
+  chart.draw(view, options);
+
+  var sh = selectHandler.bind(null, chart, data, 'spDetail.php?identifier=');
+  google.visualization.events.addListener(chart, 'select', sh);
 }
 
 function drawIdpsTable(getEl) {
-    var el = getEl();
-    if (!el) return;
-    
-    var data = new google.visualization.DataTable();
+  var el = getEl();
+  if (!el) return;
 
-    data.addColumn(
-        'string', getTranslation('tables_identity_provider')
-    );
-    data.addColumn(
-        'string', getTranslation('tables_identity_provider')
-    );
-    data.addColumn(
-        'number', getTranslation('count')
-    );
-    data.addRows(getStatisticsData('loginCountPerIdp'));
+  var data = new google.visualization.DataTable();
 
-    data.sort([{column: 2, desc: true}]);
+  data.addColumn(
+    'string', getTranslation('tables_identity_provider')
+  );
+  data.addColumn(
+    'string', getTranslation('tables_identity_provider')
+  );
+  data.addColumn(
+    'number', getTranslation('count')
+  );
+  data.addRows(getStatisticsData('loginCountPerIdp'));
 
-    var view = new google.visualization.DataView(data);
+  data.sort([{ column: 2, desc: true }]);
 
-    view.setColumns([0, 2]);
+  var view = new google.visualization.DataView(data);
 
-    var table = new google.visualization.Table(el);
+  view.setColumns([0, 2]);
 
-    table.draw(view);
+  var table = new google.visualization.Table(el);
 
-    google.visualization.events.addListener(table, 'select', selectHandler);
+  table.draw(view);
 
-    function selectHandler() {
-        var selection = table.getSelection();
-        if (selection.length) {
-            var entityId = data.getValue(selection[0].row, 1);
-            window.location.href = 'idpDetail.php?entityId=' + entityId;
-        }
-    }
+  var sh = selectHandler.bind(null, table, data, 'idpDetail.php?entityId=');
+  google.visualization.events.addListener(table, 'select', sh);
 }
 
 function drawAccessedSpsChart(getEl) {
-    var el = getEl();
-    if (!el) return;
-    
-    var data = google.visualization.arrayToDataTable([['service', 'Count']].concat(getStatisticsData('accessCountForIdentityProviderPerServiceProviders')));
+  var el = getEl();
+  if (!el) return;
 
-    var options = {
-        pieSliceText: 'value',
-        chartArea: {left: 20, top: 0, width: '100%', height: '100%'}
-    };
+  var data = google.visualization.arrayToDataTable([['service', 'Count']].concat(getStatisticsData('accessCountForIdentityProviderPerServiceProviders')));
 
-    var chart = new google.visualization.PieChart(el);
+  var options = {
+    pieSliceText: 'value',
+    chartArea: {
+      left: 20, top: 0, width: '100%', height: '100%'
+    }
+  };
 
-    data.sort([{column: 1, desc: true}]);
-    chart.draw(data, options);
+  var chart = new google.visualization.PieChart(el);
+
+  data.sort([{ column: 1, desc: true }]);
+  chart.draw(data, options);
 }
 
 function drawAccessedSpsTable(getEl) {
-    var el = getEl();
-    if (!el) return;
-    
-    var data = new google.visualization.DataTable();
+  var el = getEl();
+  if (!el) return;
 
-    data.addColumn(
-        'string',
-        getTranslation('tables_service_provider')
-    );
-    data.addColumn(
-        'number',
-        getTranslation('count')
-    );
-    data.addRows(
-        getStatisticsData('accessCountForIdentityProviderPerServiceProviders')
-    );
+  var data = new google.visualization.DataTable();
 
-    var table = new google.visualization.Table(el);
+  data.addColumn(
+    'string',
+    getTranslation('tables_service_provider')
+  );
+  data.addColumn(
+    'number',
+    getTranslation('count')
+  );
+  data.addRows(
+    getStatisticsData('accessCountForIdentityProviderPerServiceProviders')
+  );
 
-    var options = {
-        allowHtml: true
-    };
+  var table = new google.visualization.Table(el);
 
-    table.draw(data, options);
+  var options = {
+    allowHtml: true
+  };
+
+  table.draw(data, options);
 }
 
 function drawSpsTable(getEl) {
-    var el = getEl();
-    if (!el) return;
-    
-    var data = new google.visualization.DataTable();
+  var el = getEl();
+  if (!el) return;
 
-    data.addColumn(
-        'string',
-        getTranslation('tables_service_provider')
-    );
-    data.addColumn(
-        'string',
-        getTranslation('count')
-    );
-    data.addColumn(
-        'number',
-        getTranslation('count')
-    );
-    data.addRows(
-        getStatisticsData('accessCountPerService')
-    );
+  var data = new google.visualization.DataTable();
 
-    var view = new google.visualization.DataView(data);
+  data.addColumn(
+    'string',
+    getTranslation('tables_service_provider')
+  );
+  data.addColumn(
+    'string',
+    getTranslation('count')
+  );
+  data.addColumn(
+    'number',
+    getTranslation('count')
+  );
+  data.addRows(
+    getStatisticsData('accessCountPerService')
+  );
 
-    view.setColumns([0, 2]);
+  var view = new google.visualization.DataView(data);
 
-    var table = new google.visualization.Table(el);
+  view.setColumns([0, 2]);
 
-    var formatter = new google.visualization.DateFormat({pattern: "MMMM  yyyy"});
-    formatter.format(data, 0); // Apply formatter to second column
-    var options = {
-        allowHtml: true
-    };
+  var table = new google.visualization.Table(el);
 
-    table.draw(view, options);
+  var formatter = new google.visualization.DateFormat({ pattern: 'MMMM  yyyy' });
+  formatter.format(data, 0); // Apply formatter to second column
+  var options = {
+    allowHtml: true
+  };
 
-    google.visualization.events.addListener(table, 'select', selectHandler);
+  table.draw(view, options);
 
-    function selectHandler() {
-        var selection = table.getSelection();
-        if (selection.length) {
-            var identifier = data.getValue(selection[0].row, 1);
-            window.location.href = 'spDetail.php?identifier=' + identifier;
-        }
-    }
+  var sh = selectHandler.bind(null, table, data, 'spDetail.php?identifier=');
+  google.visualization.events.addListener(table, 'select', sh);
 }
 
 function drawUsedIdpsChart(getEl) {
-    var el = getEl();
-    if (!el) return;
-    
-    var data = google.visualization.arrayToDataTable([['service', 'Count']].concat(getStatisticsData('accessCountForServicePerIdentityProviders')));
+  var el = getEl();
+  if (!el) return;
 
-    var options = {
-        pieSliceText: 'value',
-        chartArea: {left: 20, top: 0, width: '100%', height: '100%'}
-    };
+  var data = google.visualization.arrayToDataTable([['service', 'Count']].concat(getStatisticsData('accessCountForServicePerIdentityProviders')));
 
-    var chart = new google.visualization.PieChart(el);
+  var options = {
+    pieSliceText: 'value',
+    chartArea: {
+      left: 20, top: 0, width: '100%', height: '100%'
+    }
+  };
 
-    data.sort([{column: 1, desc: true}]);
-    chart.draw(data, options);
+  var chart = new google.visualization.PieChart(el);
+
+  data.sort([{ column: 1, desc: true }]);
+  chart.draw(data, options);
 }
 
 function drawUsedIdpsTable(getEl) {
-    var el = getEl();
-    if (!el) return;
-    
-    var data = new google.visualization.DataTable();
+  var el = getEl();
+  if (!el) return;
 
-    data.addColumn(
-        'string',
-        getTranslation('tables_service_provider')
-    );
-    data.addColumn(
-        'number',
-        getTranslation('count')
-    );
-    data.addRows(
-        getStatisticsData('accessCountForServicePerIdentityProviders')
-    );
+  var data = new google.visualization.DataTable();
 
-    var table = new google.visualization.Table(el);
+  data.addColumn(
+    'string',
+    getTranslation('tables_service_provider')
+  );
+  data.addColumn(
+    'number',
+    getTranslation('count')
+  );
+  data.addRows(
+    getStatisticsData('accessCountForServicePerIdentityProviders')
+  );
 
-    var options = {
-        allowHtml: true
-    };
+  var table = new google.visualization.Table(el);
 
-    table.draw(data, options);
+  var options = {
+    allowHtml: true
+  };
+
+  table.draw(data, options);
 }
 
 function getterLoadCallback(getEl, callback) {
-    google.charts.setOnLoadCallback(callback.bind(null, getEl));
+  google.charts.setOnLoadCallback(callback.bind(null, getEl));
 }
 
 function classLoadCallback(className, callback) {
-    getterLoadCallback(function(){return $('.'+className+':visible')[0];}, callback);
+  getterLoadCallback(function () { return $('.' + className + ':visible')[0]; }, callback); // eslint-disable-line func-names
 }
 
 function idLoadCallback(id, callback) {
-    getterLoadCallback(document.getElementById.bind(document, id), callback);
+  getterLoadCallback(document.getElementById.bind(document, id), callback);
 }
 
 function chartInit() {
-    idLoadCallback('loginsDashboard', drawLoginsChart);
-    classLoadCallback('chart-idpsChart', drawIdpsChart);
-    classLoadCallback('chart-spsChart', drawSpsChart);
-    idLoadCallback('idpsTable', drawIdpsTable);
-    idLoadCallback('accessedSpsChartDetail', drawAccessedSpsChart);
-    idLoadCallback('accessedSpsTable', drawAccessedSpsTable);
-    idLoadCallback('spsTable', drawSpsTable);
-    idLoadCallback('usedIdPsChartDetail', drawUsedIdpsChart);
-    idLoadCallback('usedIdPsTable', drawUsedIdpsTable);
-    $("#dateSelector input[name=lastDays]").on('click', function(){
-        this.form.submit();
-    });
+  idLoadCallback('loginsDashboard', drawLoginsChart);
+  classLoadCallback('chart-idpsChart', drawIdpsChart);
+  classLoadCallback('chart-spsChart', drawSpsChart);
+  idLoadCallback('idpsTable', drawIdpsTable);
+  idLoadCallback('accessedSpsChartDetail', drawAccessedSpsChart);
+  idLoadCallback('accessedSpsTable', drawAccessedSpsTable);
+  idLoadCallback('spsTable', drawSpsTable);
+  idLoadCallback('usedIdPsChartDetail', drawUsedIdpsChart);
+  idLoadCallback('usedIdPsTable', drawUsedIdpsTable);
+  $('#dateSelector input[name=lastDays]').on('click', function submitForm() {
+    this.form.submit();
+  });
 }
 
-$(document).ready(function() {
-	google.charts.load('current', {'packages': ['corechart', 'controls', 'table']});
+$(document).ready(function docReady() {
+  google.charts.load('current', { packages: ['corechart', 'controls', 'table'] });
 
-    console.log($("#tabdiv").data('activetab'));
-    $("#tabdiv").tabs({
-        selected: $("#tabdiv").data('activetab'),
-        load: chartInit
-    });
-    chartInit();
+  $('#tabdiv').tabs({
+    selected: $('#tabdiv').data('activetab'),
+    load: chartInit
+  });
+  chartInit();
 });
