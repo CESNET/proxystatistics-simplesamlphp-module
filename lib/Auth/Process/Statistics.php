c5b5b41f1c2cdcda06a0460f59c84e4187cb2fe1
Dominik Frantisek Bucik <bucik@ics.muni.cz>
2022-02-01 10:26:52 +0100

    fix: 🐛 Fix possible exceptions and refactor code
    Refactored code to include more logging. Also fixed some possible
    exception places.

--- lib/Auth/Process/Statistics.php
+++ lib/Auth/Process/Statistics.php
@@ -5,11 +5,17 @@ declare(strict_types=1);
 namespace SimpleSAML\Module\proxystatistics\Auth\Process;
 
 use DateTime;
+use Exception;
 use SimpleSAML\Auth\ProcessingFilter;
+use SimpleSAML\Logger;
 use SimpleSAML\Module\proxystatistics\DatabaseCommand;
 
 class Statistics extends ProcessingFilter
 {
+    private const STAGE = 'proxystatistics:Statistics';
+
+    private const DEBUG_PREFIX = self::STAGE . ' - ';
+
     public function __construct($config, $reserved)
     {
         parent::__construct($config, $reserved);
@@ -19,6 +25,12 @@ class Statistics extends ProcessingFilter
     {
         $dateTime = new DateTime();
         $dbCmd = new DatabaseCommand();
-        $dbCmd->insertLogin($request, $dateTime);
+        try {
+            $dbCmd->insertLogin($request, $dateTime);
+        } catch (Exception $ex) {
+            Logger::error(
+                self::DEBUG_PREFIX . 'Caught exception while inserting login into statistics: ' . $ex->getMessage()
+            );
+        }
     }
 }
